8a2bbdf1e1855d3982c182a56c761bac1f9d49f5
Cristian <125401680+Tropezodev@users.noreply.github.com>
2024-01-15 09:39:09 +0100

    TaskJS7

---
+++ Trimestre2/Task7JS/Task7.js
@@ -0,0 +1,9 @@
+function login() {
+    let pass = prompt('Introduzca la contraseña:','')
+    if(pass=='FP2023'){
+        alert('Contraseña Correcta')
+    }
+    else{
+        alert('Contraseña Incorrecta')
+    }
+}
