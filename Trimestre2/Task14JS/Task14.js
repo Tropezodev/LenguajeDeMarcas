d793e63cdcaf6f61009bc9ba820c244ca4da6fc1
Cristian <125401680+Tropezodev@users.noreply.github.com>
2024-01-25 12:45:48 +0100

    x

--- Trimestre2/Task14JS/Task14.js
+++ Trimestre2/Task14JS/Task14.js
@@ -1,4 +1,4 @@
 function selector(){
-    let encabezado = document.querySelectorAll('h3')
+    let encabezado = document.querySelectorAll('h3') //Devuelve una Nodelist
     encabezado[1].style.color = 'red';
 }
--- Trimestre2/Task15JS/Task15.js
+++ Trimestre2/Task15JS/Task15.js
@@ -1,10 +1,11 @@
 function selector(){
-    let elemento_padre = document.querySelector('ul')
-    let elementos_hijo = document.querySelectorAll('li')
 
-    let elemento_creado = document.createElement('li')
-    let texto= document.createTextNode('Nuevo Elemento Creado')
-    elemento_creado.appendChild(texto)
+    let elemento_creado = document.createElement('li') //Se crea un elemento <li> vacío y lo guardamos en una variable
+    let texto= document.createTextNode('Nuevo Elemento Creado') //Creamos el texto que va dentro del nodo
+    elemento_creado.appendChild(texto) //Metemos el texto en el elemento <li>
 
-    elemento_padre.insertBefore(elemento_creado, elementos_hijo[1])
+    let elemento_padre = document.querySelector('ul') //Recuperamos el elemento padre
+    let elementos_hijo = document.querySelectorAll('li') //Recuperamos el Nodelist de los elementos hijo
+
+    elemento_padre.insertBefore(elemento_creado, elementos_hijo[1]) //Introducimos el elemento creado en la posición que queremos
 }
